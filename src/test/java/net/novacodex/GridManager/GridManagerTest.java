b96716c00692bb550a81ad47012504f8b498d05e
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2011-09-12 15:58:58 +0200

    Unit tests

---
+++ src/test/java/net/novacodex/GridManager/GridManagerTest.java
@@ -0,0 +1,66 @@
+package net.novacodex.GridManager;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class GridManagerTest {
+	@Test
+	public void getCellIndexTest() {
+		String cellIndex= GridManager.getCellIndex(0.1,0.3,1);
+		org.junit.Assert.assertEquals("0",cellIndex);
+		String cellIndex2= GridManager.getCellIndex(0.2,0.3,1);
+		org.junit.Assert.assertEquals("1",cellIndex2);
+
+		String cellIndex3= GridManager.getCellIndex(3,10,4);
+		org.junit.Assert.assertEquals("4",cellIndex3);
+		String cellIndex4= GridManager.getCellIndex(6,10,4);
+		org.junit.Assert.assertEquals("9",cellIndex4);
+	}
+
+	@Test
+	public void getGridCellIdTest() {
+		Point point= new Point(45,4);
+
+		String cellId= GridManager.getGridCellId(point,5);
+		org.junit.Assert.assertEquals("0|4",cellId);
+
+		String cellId2= GridManager.getGridCellId(point,7);
+		org.junit.Assert.assertEquals("2|16",cellId2);
+
+		String cellId3= GridManager.getGridCellId(point,14);
+		org.junit.Assert.assertEquals("257|2048",cellId3);
+
+		Point point2= new Point(-12,-179);
+
+		String cellId4= GridManager.getGridCellId(point2,5);
+		org.junit.Assert.assertEquals("-32|-2",cellId4);
+
+		String cellId5= GridManager.getGridCellId(point2,7);
+		org.junit.Assert.assertEquals("-125|-5",cellId5);
+
+		String cellId6= GridManager.getGridCellId(point2,14);
+		org.junit.Assert.assertEquals("-15937|-547",cellId6);
+	}
+
+	@Test public void getGridCellIdsTest() {
+		Point point= new Point(45,4);
+
+		Map<Integer,String> cellIdsByTile= GridManager.getGridCellIds(point,0,15);
+
+		org.junit.Assert.assertEquals("0|4",cellIdsByTile.get(5));
+		org.junit.Assert.assertEquals("2|16",cellIdsByTile.get(7));
+		org.junit.Assert.assertEquals("257|2048",cellIdsByTile.get(14));
+	}
+
+	@Test
+	public void findBestTileLevelForSearchRangeTest() {
+		int bestTileLevel= GridManager.findBestTileLevelForSearchRange(50);
+
+		org.junit.Assert.assertEquals(9,bestTileLevel);
+
+		int bestTileLevel2= GridManager.findBestTileLevelForSearchRange(1);
+
+		org.junit.Assert.assertEquals(15,bestTileLevel2);
+	}
+}
---
+++ src/test/java/net/novacodex/GridManager/PointTest.java
@@ -0,0 +1,32 @@
+package net.novacodex.GridManager;
+
+import org.junit.Test;
+
+public class PointTest {
+	@Test
+	public void normalizeTest() {
+		Point point= new Point(45,517);
+		org.junit.Assert.assertTrue(point.getLatitude()==45);
+		org.junit.Assert.assertTrue(point.getLongitude()==157);
+	}
+
+	@Test
+	public void computeDestinationTest() {
+		Point point= new Point(45,4);
+
+		Point destination= point.computeDestination( 100, 45);
+
+		org.junit.Assert.assertEquals(Math.toRadians(destination.getLatitude()),0.796432523,0.00001);
+		org.junit.Assert.assertEquals(Math.toRadians(destination.getLongitude()),0.08568597,0.00001);
+	}
+
+	@Test
+	public void distanceToPoint() {
+		Point point= new Point(45,4);
+		Point point2= new Point(46,14);
+
+		double distance= point.distanceToPoint(point2);
+
+		org.junit.Assert.assertEquals(distance,786.7,0.1);
+	}
+}
