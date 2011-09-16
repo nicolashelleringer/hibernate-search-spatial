0afe0241cb1f8f63bdd7e88ff249de471a5bd3c3
Mathieu Perez <mathieu.perez@novacodex.net>
2011-09-16 12:09:21 +0200

    useless access modifiers

--- src/main/java/net/novacodex/hibernate/search/spatial/SpatialIndexable.java
+++ src/main/java/net/novacodex/hibernate/search/spatial/SpatialIndexable.java
@@ -1,6 +1,7 @@
 package net.novacodex.hibernate.search.spatial;
 
 public interface SpatialIndexable {
-	public double getLatitude();
-	public double getLongitude();
+	double getLatitude();
+
+	double getLongitude();
 }
