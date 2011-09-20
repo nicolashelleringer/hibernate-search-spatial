e53cfd370aedfb11811ff26a8ebe1d19f89e58d3
Nicolas Helleringer <nicolas.helleringer@novacodex.net>
2011-09-20 17:54:37 +0200

    Implemented ParameterizedBridge on SpatialFieldBridge

--- src/main/java/net/novacodex/hibernate/search/spatial/SpatialFieldBridge.java
+++ src/main/java/net/novacodex/hibernate/search/spatial/SpatialFieldBridge.java
@@ -6,13 +6,17 @@ import org.apache.lucene.document.NumericField;
 
 import org.hibernate.search.bridge.FieldBridge;
 import org.hibernate.search.bridge.LuceneOptions;
+import org.hibernate.search.bridge.ParameterizedBridge;
 
 import java.util.Map;
 
-public class SpatialFieldBridge implements FieldBridge {
+public class SpatialFieldBridge implements FieldBridge, ParameterizedBridge {
 
 	public static final int MIN_GRID_LEVEL = 0;
 	public static final int MAX_GRID_LEVEL = 16;
+
+	private int min_grid_level= MIN_GRID_LEVEL;
+	private int max_grid_level= MAX_GRID_LEVEL;
 
 	@Override
 	public void set(String name, Object value, Document document, LuceneOptions luceneOptions) {
@@ -22,9 +26,9 @@ public class SpatialFieldBridge implements FieldBridge {
 
 			Point point = Point.fromDegrees( spatialIndexable.getLatitude(), spatialIndexable.getLongitude() );
 
-			Map<Integer, String> cellIds = GridManager.getGridCellsIds( point, MIN_GRID_LEVEL, MAX_GRID_LEVEL );
+			Map<Integer, String> cellIds = GridManager.getGridCellsIds( point, min_grid_level, max_grid_level );
 
-			for ( int i = MIN_GRID_LEVEL; i <= MAX_GRID_LEVEL; i++ ) {
+			for ( int i = min_grid_level; i <= max_grid_level; i++ ) {
 				document.add(
 						new Field(
 								FieldUtils.formatFieldName( i, name ),
@@ -46,6 +50,18 @@ public class SpatialFieldBridge implements FieldBridge {
 							point.getLongitude()
 					)
 			);
+		}
+	}
+
+	@Override
+	public void setParameterValues(Map parameters) {
+		Object min_grid_level= parameters.get("min_grid_level");
+		if(min_grid_level instanceof Integer) {
+			this.min_grid_level= (Integer)min_grid_level;
+		}
+		Object max_grid_level= parameters.get("min_grid_level");
+		if(max_grid_level instanceof Integer) {
+			this.max_grid_level= (Integer)max_grid_level;
 		}
 	}
 }
